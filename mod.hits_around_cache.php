b4e2c4c9b088c9a14c35e017db2801f705e6987d
Andy Hebrank <ahebrank@gmail.com>
2014-12-22 07:10:55 -0500

    action and template tag

--- mod.hits_around_cache.php
+++ mod.hits_around_cache.php
@@ -45,6 +45,42 @@ class Hits_around_cache {
   public function __construct() {
   }
   
+   /**
+   * Register a hit from the front end
+   *
+   * @return  boolean   TRUE
+   */ 
+  function frontend_hit() {
+    // record the URL title
+    // hopefully the timestamping is automatic via mysql
+    $url_title = ee()->input->get('url_title', $xss_clean = true);
+    ee()->db->insert('hits_ac', array('url_title' => $url_title));
+
+    return true;
+  }
+
+  /**
+   * Count hits to a URL for a date range
+   *
+   * @return  int   number of hits
+   */ 
+  function hit_count() {
+    // two tag parameters:
+    //    url_title : defines the URL
+    //    previous : how far back to go, in PHP strtotime form
+    //               default to '-1 month'
+    if (($url_title = ee()->TMPL->fetch_param('url_title')) === false) return;
+    $previous = ee()->TMPL->fetch_param('previous');
+    $previous = ($previous === false)? "-1 month":$previous;
+
+    $start_time = date('Y-m-d H:i:s', strtotime($previous));
+    $q = ee()->db->select('COUNT(hit_it) count')
+            ->from('hits_ac')
+            ->where('url_title', $url_title)
+            ->where('timestamp >=', $start_time)
+            ->get();
+    return $q->row('count');
+  }
   
   
 }
